17991d94851430dd0f68c572b1bf6366dc1435c0
Javier Berrocal <berrocalirl@gmail.com>
2019-03-18 11:44:45 +0000

    Moved triad-website to root folder for usability

--- mkdockerize.sh
+++ mkdockerize.sh
@@ -2,8 +2,6 @@
 
 OPTION=$1
 
-cd mkdocs
-
 if [ "$OPTION" = "produce" ];
 then
    rm -rf output
@@ -12,10 +10,8 @@ else
    if [ "$OPTION" = "serve" ];
       then
          mkdir -p input && rm -rf input/*
-         cd triad-website && tar cfz site.tar.gz * && cd ..
+         cd triad-website && rm -rf site.tar.gz && tar cfz site.tar.gz * && cd ..
          mv triad-website/site.tar.gz ./input/
          docker run -p 8000:8000 -d -v ${PWD}/input:/tmp mkdocs serve
    fi
 fi
-
-cd ..
