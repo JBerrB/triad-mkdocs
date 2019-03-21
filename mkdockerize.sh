9953aa6d282d8f47bdb8a5485a91b0fec0fcb0e3
Javier Berrocal <berrocalirl@gmail.com>
2019-03-21 22:20:45 +0000

    Added stop OPTION to mkdockerize.sh

--- mkdockerize.sh
+++ mkdockerize.sh
@@ -2,22 +2,21 @@
 
 OPTION=$1
 
-if [ "$OPTION" = "produce" ];
-then
+if [ "$OPTION" = "produce" ];then
    rm -rf output
    docker run -d -v ${PWD}/output:/tmp mkdocs produce
 else
-   if [ "$OPTION" = "serve" ];
-   then
+   if [ "$OPTION" = "serve" ];then
       mkdir -p input && rm -rf input/*
       cd triad-website && rm -rf site.tar.gz && tar cfz site.tar.gz * && cd ..
       mv triad-website/site.tar.gz ./input/
       docker run -p 8000:8000 -d -v ${PWD}/input:/tmp mkdocs serve
    else
-      if [ "$OPTION" = "stop" ];
-      then
+      if [ "$OPTION" = "stop" ];then
          export CONTAINER_ID=$(docker ps | grep mkdocs | awk '{print $1}')
-         docker stop $CONTAINER_ID
+         if [ "$CONTAINER_ID" != "" ];then
+            docker stop $CONTAINER_ID
+         fi
       fi
    fi
 fi
