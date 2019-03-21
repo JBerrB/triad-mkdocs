3c41146be3f2a5f301ef64fcac2ac3cb84c23fac
Javier Berrocal <berrocalirl@gmail.com>
2019-03-21 21:45:43 +0000

    Added stop option to stop mkdocs container when running

--- mkdockerize.sh
+++ mkdockerize.sh
@@ -8,10 +8,16 @@ then
    docker run -d -v ${PWD}/output:/tmp mkdocs produce
 else
    if [ "$OPTION" = "serve" ];
+   then
+      mkdir -p input && rm -rf input/*
+      cd triad-website && rm -rf site.tar.gz && tar cfz site.tar.gz * && cd ..
+      mv triad-website/site.tar.gz ./input/
+      docker run -p 8000:8000 -d -v ${PWD}/input:/tmp mkdocs serve
+   else
+      if [ "$OPTION" = "stop" ];
       then
-         mkdir -p input && rm -rf input/*
-         cd triad-website && rm -rf site.tar.gz && tar cfz site.tar.gz * && cd ..
-         mv triad-website/site.tar.gz ./input/
-         docker run -p 8000:8000 -d -v ${PWD}/input:/tmp mkdocs serve
+         export CONTAINER_ID=$(docker ps | grep mkdocs | awk '{print $1}')
+         docker stop $CONTAINER_ID
+      fi
    fi
 fi
